c01ab8b94fa56bbc69ce326a7e4d9200a3ab153b
Edivânia Pontes <edivaniapts@gmail.com>
2021-08-03 01:13:17 -0300

    projeto #4 final - icon + inicio da home screen

--- ilovetv_flutter/lib/main.dart
+++ ilovetv_flutter/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Home());
+  runApp(
+    MaterialApp(
+      home: Home(),
+    )
+  );
 }
 
 class Home extends StatefulWidget {
@@ -14,8 +19,37 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return new Scaffold(
+      appBar: AppBar(
+        title: Text('I love TV',  style: TextStyle(color: Colors.black),),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+
+      body: Container(
+        color: Colors.purple.shade800,
+        child: Center(
+          child: Text(
+            "App em construção...",
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.purple.shade800,
+        height: 50,
+        index: 1,
+        animationDuration: Duration( milliseconds: 500 ),
+        items: <Widget>[
+          Icon(Icons.add, size: 25),
+          Icon(Icons.home, size: 25),
+          Icon(Icons.compare_arrows, size: 25),
+        ],
+        onTap: (index) {
+          debugPrint(">> Index do curved_navigation_bar: $index");
+        },
+      ),
     );
   }
 }
