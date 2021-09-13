21b4d9ba313e7e45b1dace915252a5e76257b742
Edivânia Pontes <edivaniapts@gmail.com>
2021-09-12 23:11:59 -0300

    projeto #4 season model

--- ilovetv_flutter/lib/src/model/season.dart
+++ ilovetv_flutter/lib/src/model/season.dart
@@ -1,3 +1,19 @@
 class Season {
-  
+  final int id;
+  final int number;
+  final int ep_count;
+  final String name;
+
+  Season(
+    this.id,
+    this.number,
+    this.ep_count,
+    this.name,
+  );
+
+  Season.fromJson(Map<String, dynamic> json)
+    : id = json["id"],
+      number = json["number"],
+      ep_count = json["episode_count"],
+      name = json["name"];
 }
